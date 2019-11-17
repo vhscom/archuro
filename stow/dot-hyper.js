7378aefefa56fcae7ec371829059a49fe8f5238f
vhs <0xc000007b@tutanota.com>
2019-11-17 11:11:03 +0800

    chore: add verminal theme for hyper

--- stow/dot-hyper.js
+++ stow/dot-hyper.js
@@ -170,7 +170,13 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperpower", "hyper-custom-touchbar", "hypercwd", "hyper-flat"],
+  plugins: [
+    "hyperpower",
+    "hyper-custom-touchbar",
+    "hypercwd",
+    "hyper-flat",
+    "verminal"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
